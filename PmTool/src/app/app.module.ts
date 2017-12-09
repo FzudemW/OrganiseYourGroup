7b5d82125f41c32ad11c775d65def341e863a1c2
Manuel Araujo <mr353465@fh-muenster.de>
2017-12-09 15:37:27 +0100

    Routing im PM-Tool2

--- PmTool/src/app/app.module.ts
+++ PmTool/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { appRouting, routingComponents } from './app.routing';
 
 
 import { AppComponent } from './app.component';
-
+import { ProductBacklogComponent } from './navigationBar/product-backlog/product-backlog.component';
+import { ScrumBoardComponent } from './navigationBar/scrum-board/scrum-board.component';
+import { PlanningPokerComponent } from './navigationBar/planning-poker/planning-poker.component';
+import { BurnDownChartComponent } from './navigationBar/burn-down-chart/burn-down-chart.component';
+import { RoadmapComponent } from './navigationBar/roadmap/roadmap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductBacklogComponent,
+    ScrumBoardComponent,
+    PlanningPokerComponent,
+    BurnDownChartComponent,
+    RoadmapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    appRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
